b1d4aeba9a28a8fb60ea426f36adf4a46473ef8b
Aleksandr <daaarkzs47@gmail.com>
2025-06-29 17:20:27 +0500

    Project settings update

--- src/server/hash.ts
+++
@@ -1,8 +0,0 @@
-import bcrypt from 'bcrypt';
-
-const password = process.argv[2]; // берём пароль из аргументов
-const saltRounds = 10;
-
-bcrypt.hash(password, saltRounds).then((hash: string) => {
-    console.log('Хэш:', hash);
-});1

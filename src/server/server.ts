f93b127001d60b9b5f1b9bf502ce87301cc28708
Aleksandr <daaarkzs47@gmail.com>
2025-06-29 23:13:02 +0500

    server update

--- src/server/server.ts
+++ src/server/server.ts
@@ -2,26 +2,26 @@ import express, { Request, Response, NextFunction } from 'express';
 import bodyParser from 'body-parser';
 
 import { UserService } from './core/services/UserService/UserService';
-import { UserController } from './infrastructure/controllers/User/UserController';
-import { UserRepositoryImpl } from './infrastructure/db/repository/UserRepositoryImpl';
+import { HttpUserController } from './infrastructure/controllers/User/HttpUserController';
+import { UserRepositoryJSON } from './infrastructure/db/repository/UserRepositoryJSON';
 import pinoHttp from 'pino-http';
 import { logger } from './logger'
 
 export const startServer = () => {
-    const userRepository = new UserRepositoryImpl('./users.json');
+    const userRepository = new UserRepositoryJSON('./users.json');
     const userService = new UserService(userRepository);
-    const userController = new UserController(userService);
+    const httpUserController = new HttpUserController(userService);
 
     const app = express();
     app.use(pinoHttp({ logger }));
     app.use(bodyParser.json());
 
     app.post('/users', (req: Request, res: Response) => {
-        userController.createUser(req, res);
+        httpUserController.createUser(req, res);
     });
 
     // app.get('/users', (req: Request, res: Response) => {
-    //     userController.getAll(req, res);
+    //     httpUserController.getAll(req, res);
     // });
 
     const port = process.env.PORT || 3000;

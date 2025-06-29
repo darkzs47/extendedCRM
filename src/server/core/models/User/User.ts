cc0476240045c98da953d4afc8b4eab800564b9f
Aleksandr <daaarkzs47@gmail.com>
2025-06-29 16:44:08 +0500

    ÐFlder structure

---
+++ src/server/core/models/User/User.ts
@@ -0,0 +1,13 @@
+export class User {
+    constructor(
+        id: number,
+        secondName: string,
+        name: string,
+        lastName: string,
+        email: string,
+        phone: string,
+        password: string,
+        role: string,
+        supplierID?: number,
+    ) { }
+}
